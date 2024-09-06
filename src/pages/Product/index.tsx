1b6b902ff98e9af09f9117ed4c528094e8318076
João Colares <joaocolaresm@gmail.com>
2024-09-06 20:19:27 -0300

    aula_3

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom"
 import Hero from "../../components/Hero"
+import Section from "../../components/Section"
 
 const Product = () => {
     const {id} = useParams()
@@ -7,6 +8,23 @@ const Product = () => {
     return (
       <>
         <Hero/>
+        <Section title="Sobre o jogo" background="black">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Eum dolores veniam voluptas consectetur placeat.
+            Deleniti voluptatum non repudiandae laudantium, quidem dignissimos harum maiores delectus, rem unde ipsa porro eos odio.
+          </p>
+        </Section>
+        <Section title="Mais detalhes" background="gray">
+          <p>
+            <b>Plataforma</b>: PlayStation 5<br/>
+            <b>Desenvolvedor:</b> Avargrg egerbe<br/>
+            <b>Editora:</b> jrgejbgkjbejvbejnejvnejvnejke<br/>
+            <b>Idiomas:</b> jgkwjbkjvkjv kjv sfjkv kjv kjvs jv <br/>        </p>
+        </Section>
+        <Section title="Galeria" background="black">
+          <div>fotos</div>
+        </Section>
       </>
     )
 }
